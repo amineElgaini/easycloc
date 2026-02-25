a4cda6319ef49ff4b216db5ac3cb2a6ad0d55a04
amineElgaini <elgainiamine@gmail.com>
2026-02-25 15:02:16 +0000

    initialize breeze, create migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,13 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
+            $table->string('profile_image')->nullable();
+            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->integer('reputation')->default(0);
+            $table->boolean('is_banned')->default(false);
+            $table->timestamp('banned_at')->nullable();
+            
             $table->rememberToken();
             $table->timestamps();
         });
